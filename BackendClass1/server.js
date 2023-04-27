18d940bf369226846340608c56123e4d6f92cc9a
Vikas Kumar Singh <vikaskumarsingh0931@gmail.com>
2023-04-27 10:58:53 +0530

    Add files via upload

---
+++ BackendClass1/server.js
@@ -0,0 +1,36 @@
+//server initialte
+const express = require('express');
+const app = express();
+
+//used to res.body 
+const bodyParser = require("body-parser");
+ app.use(bodyParser.json());
+
+
+//activate the server port no 3000
+ app.listen(3000,()=>{
+    console.log("Sever started at port no 3000");
+ });
+
+ //Routes
+app.get("/",(request,respond)=>{
+   respond.send("To Kaise ho app sab sare ke sare");
+})
+
+//
+app.post("/api/cars",(request ,respond)=>{
+   const {name,brand} = request.body;
+   console.log(name);
+   console.log(brand);
+   respond.send("Cards run successfully.");
+
+})
+//import mongoose for interface b/w mongodb and node js
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/myDatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(()=> { console.log("Connection Successfull")})
+.catch(()=>console.log("Recieved an error"));
